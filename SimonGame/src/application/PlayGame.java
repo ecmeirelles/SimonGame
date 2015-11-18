e5545097737f1aed7a78461fdb163b1a9371e5eb
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2015-11-18 04:04:29 +0000

    Creating a main class

---
+++ SimonGame/src/application/PlayGame.java
@@ -0,0 +1,11 @@
+package application;
+
+public class PlayGame {
+   
+    public static void main(String[] args) {
+       Game game = new Game();
+       game.setVisible(true);
+
+       game.playGame();  
+    }
+}
