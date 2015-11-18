843285564cb097693856f5c1c8ecee79fee7ae06
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2015-11-18 04:05:02 +0000

    Creating a function class

---
+++ SimonGame/src/application/GameFunction.java
@@ -0,0 +1,20 @@
+package application;
+
+/* Class that represents the logic of the game */
+public class GameFunction {
+	/* SimonSays attributes */
+	public Game game;
+	
+	/* Constructor to receive an instance of Game */
+	public GameFunction(Game simonGame) {
+		this.game = simonGame;
+	}
+	
+	public void computerTurn() {
+		//TODO: Method
+	}
+	
+	public void playerTurn() {
+		//TODO: Method
+	}
+}
