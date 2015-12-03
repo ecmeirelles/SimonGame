91723af15efed5f92ad163b7ff32aea00472ba96
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2015-12-03 16:33:57 +0000

    Adding internationalization to Welcome page

--- SimonGame/src/application/Welcome.java
+++ SimonGame/src/application/Welcome.java
@@ -12,9 +12,14 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import internationalization.Internationalization;
 
 /* Class that represents the first window of the game, where all settings are chosen */
 public class Welcome extends JFrame {
@@ -26,6 +31,10 @@ public class Welcome extends JFrame {
 	private JLabel gameDescription;
 	private JLabel gameDifficultyLabel;
 	private JLabel gameThemeLabel;
+	private JLabel gameLanguage;
+	private JLabel easyLabel;
+	private JLabel mediumLabel;
+	private JLabel hardLabel;
 	private JComboBox<String> themeComboBox;
 	private JSlider difficultySlider;
 	/* Component to add labels into the JSlider */
@@ -35,11 +44,15 @@ public class Welcome extends JFrame {
 	private JRadioButton soundOff;
 	private ButtonGroup soundGroup;
 	private JButton startButton;
+	private JList<String> languageList;
 	
 	private String themes[] = {"Green/Red/Blue/Yellow", "Magenta/Cyan/Orange/Gray"};
+	private String languages[] = {"English", "Português"};
 	private int themeChosen;
 	private int difficultyChosen;
 	private int soundChosen;
+	
+	private Internationalization internationalization = new Internationalization();
 	
 	/* Constants to standardize all fonts */
 	public final String FONT_TITLE = "Andalus";
@@ -81,24 +94,86 @@ public class Welcome extends JFrame {
 		gameDescription.setBounds(50, 300, 400, 30);
 		add(gameDescription);
 		
+		/* Create a label (Language) and add it in the frame */
+		gameLanguage = new JLabel("LANGUAGE:");
+		gameLanguage.setFont(new Font(FONT_TITLE, Font.BOLD, 18));
+		gameLanguage.setForeground(Color.BLUE);
+		gameLanguage.setBounds(270, 400, 200, 30);
+		add(gameLanguage);
+		
+		/* Create a list of strings (with the languages) and add it in the frame */
+		languageList = new JList<String>(languages);
+		languageList.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
+		languageList.setBounds(420, 400, 250, 60);
+		languageList.setBackground(getBackground());
+		languageList.setSelectedIndex(0);
+		languageList.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				/* Translate the gameDescription text using the getTranslation method, word by word */
+				gameDescription.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "Customize") + " " + 
+						internationalization.getTranslation(languageList.getSelectedIndex(), "your") + " " +
+				        internationalization.getTranslation(languageList.getSelectedIndex(), "own") + " " + 
+						internationalization.getTranslation(languageList.getSelectedIndex(), "game") + ":");
+				/* Translate the gameLanguage text using the getTranslation method */
+				gameLanguage.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "LANGUAGE") + ":");
+				/* Translate the gameThemeLabel text using the getTranslation method */
+				gameThemeLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "THEME") + ":");
+				/* Translate the difficultyLabel text using the getTranslation method */
+				gameDifficultyLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "DIFFICULTY") + ":");
+				
+				/* To translate the lableTabel, first of all it is needed to set paintLabels as false */
+				difficultySlider.setPaintLabels(false);
+				/* Translate the difficulties texts using the getTranslation method */
+				easyLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "Easy"));
+				mediumLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "Medium"));
+				hardLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "Hard"));
+				/* Add it to the HashTable and add it to the labelTable */
+				difficulties.put(new Integer(10), easyLabel);
+				difficulties.put(new Integer(20), mediumLabel);
+				difficulties.put(new Integer(30), hardLabel);
+				difficultySlider.setLabelTable(difficulties);
+				/* Finally, set paintLabels as true again */
+				difficultySlider.setPaintLabels(true);
+				
+				/* Translate the soundLabel text using the getTranslation method */
+				soundLabel.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "SOUND") + ":");
+				/* Translate soundOn and soundOff texts using the getTranslation method */
+				soundOn.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "On"));
+				soundOff.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "Off"));
+				
+				/* To translate the themeComboBox elements, first of all it is needed to set remove them */
+				themeComboBox.removeAllItems();
+				/* And translate the elements text using the getTranslation method */
+				themeComboBox.addItem(internationalization.getTranslation(languageList.getSelectedIndex(), "Green/Red/Blue/Yellow"));
+				themeComboBox.addItem(internationalization.getTranslation(languageList.getSelectedIndex(), "Magenta/Cyan/Orange/Gray"));	
+				
+				/* Translate the startButton text using the getTranslation method */
+				startButton.setText(internationalization.getTranslation(languageList.getSelectedIndex(), "START"));
+				
+			}
+		});
+		add(languageList);
+		
 		/* Create a label (Theme) and add it in the frame */
 		gameThemeLabel = new JLabel("THEME:");
 		gameThemeLabel.setFont(new Font(FONT_TITLE, Font.BOLD, 18));
 		gameThemeLabel.setForeground(Color.BLUE);
-		gameThemeLabel.setBounds(270, 400, 100, 30);
+		gameThemeLabel.setBounds(270, 500, 100, 30);
 		add(gameThemeLabel);
 		
 		/* Create a combo box of strings (with the themes) and add it in the frame */
 		themeComboBox = new JComboBox<String>(themes);
 		themeComboBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
-		themeComboBox.setBounds(400, 400, 200, 30);
+		themeComboBox.setBounds(420, 500, 250, 30);
 		add(themeComboBox);
 		
 		/* Create a label (Difficulty) and add it in the frame */
 		gameDifficultyLabel = new JLabel("DIFFICULTY:");
 		gameDifficultyLabel.setFont(new Font(FONT_TITLE, Font.BOLD, 18));
 		gameDifficultyLabel.setForeground(Color.BLUE);
-		gameDifficultyLabel.setBounds(270, 500, 150, 30);
+		gameDifficultyLabel.setBounds(270, 600, 150, 30);
 		add(gameDifficultyLabel);
 		
 		/* Create an horizontal slider, starting in 10 and ending in 30, and add it in the frame */
@@ -107,14 +182,18 @@ public class Welcome extends JFrame {
 		difficultySlider.setMajorTickSpacing(10);
 		difficultySlider.setPaintTicks(true);
 		difficultySlider.setFont(new Font(FONT_BODY, Font.BOLD, 18));
-		difficultySlider.setBounds(400, 500, 200, 50);
+		difficultySlider.setBounds(420, 600, 250, 50);
 		add(difficultySlider);
 		
 		/* Create labels (Easy, Medium and Hard) and add it in the JSlider */
+		easyLabel = new JLabel("Easy");
+		mediumLabel = new JLabel("Medium");
+		hardLabel = new JLabel("Hard");
+		
 		difficulties = new Hashtable<Integer, JLabel>();
-		difficulties.put(new Integer(10), new JLabel("Easy"));
-		difficulties.put(new Integer(20), new JLabel("Medium"));
-		difficulties.put(new Integer(30), new JLabel("Hard"));
+		difficulties.put(new Integer(10), easyLabel);
+		difficulties.put(new Integer(20), mediumLabel);
+		difficulties.put(new Integer(30), hardLabel);
 		/* Allow the slider to show the labels */
 		difficultySlider.setLabelTable(difficulties);
 		difficultySlider.setPaintLabels(true);
@@ -123,13 +202,13 @@ public class Welcome extends JFrame {
 		soundLabel = new JLabel("SOUND:");
 		soundLabel.setFont(new Font(FONT_TITLE, Font.BOLD, 18));
 		soundLabel.setForeground(Color.BLUE);
-		soundLabel.setBounds(270, 600, 100, 30);
+		soundLabel.setBounds(270, 700, 100, 30);
 		add(soundLabel);
 		
 		/* Create a radio button (On) and add it in the frame */
 		soundOn = new JRadioButton("On");
 		soundOn.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
-		soundOn.setBounds(400, 600, 100, 30);
+		soundOn.setBounds(445, 700, 100, 30);
 		/* It is true by default */
 		soundOn.setSelected(true);
 		add(soundOn);
@@ -137,7 +216,7 @@ public class Welcome extends JFrame {
 		/* Create a radio button (Off) and add it in the frame */
 		soundOff = new JRadioButton("Off");
 		soundOff.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
-		soundOff.setBounds(530, 600, 100, 30);
+		soundOff.setBounds(555, 700, 100, 30);
 		add(soundOff);
 		
 		/* Allow just one radio button to be selected at a time */
@@ -148,7 +227,7 @@ public class Welcome extends JFrame {
 		/* Create a button (Start) and add it in the frame */
 		startButton = new JButton("START");
 		startButton.setFont(new Font(FONT_BODY, Font.BOLD, 16));
-		startButton.setBounds(400, 720, 100, 30);
+		startButton.setBounds(380, 780, 200, 30);
 		startButton.addActionListener(new ActionListener() {
 			
 			@Override
--- SimonGame/src/internationalization/Internationalization.java
+++ SimonGame/src/internationalization/Internationalization.java
@@ -5,22 +5,31 @@ import java.util.ResourceBundle;
 
 /* Class that represents the usage of internationalization */
 public class Internationalization {
-	
-	public String getLanguage(int index, String needTranslation) {
+	/* Method to get the word and translate it */
+	public String getTranslation(int index, String needTranslation) {
 		String word = "";
 		
+		/* If language selected is English */
 		if(index == 0) {
+			/* Locale is English from Ireland */
 			Locale locale = new Locale("en", "IE");
+			/* Use the English property file to translate */
 			ResourceBundle resourceBundle = ResourceBundle.getBundle("internationalization/en_IE", locale);
+			/* Get the word which needs translation and translate it */
 			word = resourceBundle.getString(needTranslation);
 		}
 		
+		/* If language selected is Portuguese */
 		else if(index == 1) {
+			/* Locale is Portuguese from Brazil */
 			Locale locale = new Locale("pt", "BR");
+			/* Use the Portuguese property file to translate */
 			ResourceBundle resourceBundle = ResourceBundle.getBundle("internationalization/pt_BR", locale);
+			/* Get the word which needs translation and translate it */
 			word = resourceBundle.getString(needTranslation);
 		}
 		
+		/* Return the new word */
 		return word;
 	}
 }
